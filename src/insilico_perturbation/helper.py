352474482e1eae4864ebd3bfe2e7b76d335b8521
jalil nourisa <jalil.nourisa@gmail.com>
2025-05-29 15:43:08 +0200

    working on multicolinearity in aging clock

--- src/insilico_perturbation/helper.py
+++ src/insilico_perturbation/helper.py
@@ -64,9 +64,10 @@ def perturb_tf(adata, tfs, slope_df, years=10):
     return adata_perturb
 def experiment_perturb_tfs(dataset, cell_type, data_type, tfs, n_donors=20, reg_type='ridge'):
     from ciim.src.clock.helper import prepare_input, predict_age
+    from ciim.src.common import save_dir
     import anndata as ad
     # - prepare the input and select donors 
-    adata = prepare_input(dataset, cell_type, feature_type='tf_activity', data_type=data_type)
+    adata = ad.read_h5ad(f"{save_dir}/tf_activity_smoothed/{dataset}_{cell_type}_{data_type}.h5ad")
     adata.obs['donor_age'] = adata.obs['donor_id'].astype(str) + '_' + adata.obs['age'].astype(str)
     donors = adata.obs['donor_age'].unique()
     np.random.seed(0)
@@ -116,3 +117,33 @@ def experiment_perturb_tfs(dataset, cell_type, data_type, tfs, n_donors=20, reg_
     df_pivot['dataset'] = dataset
 
     return df_pivot
+def perform_stat_test(df_pivot, ctr='baseline', treatment='perturb'):
+    
+    t_stat, p_value = stats.ttest_rel(df_pivot[treatment], df_pivot[ctr])
+    slope = (df_pivot[treatment] - df_pivot[ctr]).mean()
+    return p_value, slope
+def plot_age_acceleration_donors(df_pivot, p_value, slope, ctr='baseline', treatment='perturb'):
+    if False: # line plot
+        fig, ax = plt.subplots(figsize=(4, 4))
+        sns.scatterplot(data=df_pivot, x=ctr, y=treatment, alpha=0.7, ax=ax)
+        min_age, max_age = df_pivot[ctr].min(), df_pivot[treatment].max()
+        ax.plot([min_age, max_age], [min_age, max_age], color='gray', linestyle='--', label='Ideal')
+
+    if True: # donor plot
+        df_plot = df_pivot.reset_index().melt(id_vars='donor_age', 
+                                            value_vars=[ctr, treatment],
+                                            var_name='condition', 
+                                            value_name='predicted_age')
+
+        # plt.figure(figsize=(3, 2.5))
+        fig, ax = plt.subplots(figsize=(2.5, 2))
+        sns.lineplot(data=df_plot, x='condition', y='predicted_age', 
+                    hue='donor_age', marker='o', alpha=0.6, legend=False, ax=ax)
+        ax.margins(x=.1, y=0.1)
+        if p_value<0.05:
+            star = '*'
+        else:
+            star = ''
+        sign = '+' if slope > 0 else '-'
+        plt.title(f"{cell_type} \n Age acceleration: ({sign}) {slope:.2f} \n p: {p_value:.1} {star}")
+        plt.ylabel("Predicted age")
