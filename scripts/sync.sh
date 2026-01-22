857045adf6977b1aa9fde274d5a5657ac3f3adac
Jalil Nourisa <jalil.nourisa@gmail.com>
2026-01-22 10:02:13 +0100

    cell markers updated

--- scripts/sync.sh
+++ scripts/sync.sh
@@ -6,8 +6,8 @@
 # aws s3 cp  /vol/projects/jnourisa/hiara/datasets/sc/op.h5ad s3://openproblems-data/resources/grn/temp/datasets/sc/
 
 
-aws s3 sync  s3://openproblems-data/resources/grn/temp/datasets/bulk/ /Users/jno24/Documents/projs/ongoing/hiara/base_folder/datasets/bulk/
+# aws s3 sync  s3://openproblems-data/resources/grn/temp/datasets/bulk/ /Users/jno24/Documents/projs/ongoing/hiara/base_folder/datasets/bulk/
 # aws s3 sync  s3://openproblems-data/resources/grn/temp/output/grns /Users/jno24/Documents/projs/ongoing/hiara/base_folder/output/grns
 
-# aws s3 cp  s3://openproblems-data/resources/grn/temp/datasets/sc/op.h5ad  /Users/jno24/Documents/projs/ongoing/hiara/base_folder/datasets/sc/
+aws s3 cp  s3://openproblems-data/resources/grn/temp/datasets/sc/op.h5ad  /Users/jno24/Documents/projs/ongoing/hiara/base_folder/datasets/sc/
 # aws s3 cp  s3://openproblems-data/resources/grn/temp/datasets/sc/CXCL9.h5ad  /Users/jno24/Documents/projs/ongoing/hiara/base_folder/datasets/sc/
