2e91eb51e5280e7bc208caf139fa0273cd750ade
Jalil Nourisa <jalil.nourisa@gmail.com>
2026-02-22 06:19:18 +0100

    agentic moved elsewhere

--- src/agentic/agents/__init__.py
+++
@@ -1,1 +0,0 @@
-"""Agents package for omics and biology interpretation."""
--- src/agentic/test_sle.py
+++
@@ -1,4 +0,0 @@
-from helper import get_disease_signature
-
-result = get_disease_signature('SLE', 'CD8T', 'tfa_major_b')
-print(result)
