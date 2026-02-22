8453309a81de11de6afcb4652d2ccef153b27d9e
jalil nourisa <jalil.nourisa@gmail.com>
2026-02-22 02:46:31 +0100

    fixing preprocessing issues

--- src/process_dataset/bulkify/script.py
+++ src/process_dataset/bulkify/script.py
@@ -49,14 +49,12 @@ if __name__ == '__main__':
     adata = ad.read_h5ad(args.sc_dataset_file)
     dataset = adata.obs['dataset'].unique()[0]
     cfg = get_config(dataset)
-    pseudobulk_group = cfg.pseudobulk_group
+    bulk_group = cfg.bulk_group
+    covariate_major = bulk_group + [MAJOR_CT_LABEL]
 
     # - main bulk data (per major celltype)
     print('Bulkifying main cell types')
-    
-    
-
-    adata_bulk_major_celltypes = bulkify_func(adata, covariates=pseudobulk_group)
+    adata_bulk_major_celltypes = bulkify_func(adata, covariates=covariate_major)
     adata_bulk_major_celltypes = normalize(adata_bulk_major_celltypes)
     low_cells = adata_bulk_major_celltypes.obs['cell_count'] < 10
     print(f'Dropping {low_cells.sum()} bulk samples with less than 10 cells')
@@ -66,8 +64,7 @@ if __name__ == '__main__':
 
     # - bulk minor
     print('Bulkifying minor cell types')
-    covariates_minor = pseudobulk_group.copy()
-    covariates_minor.append(SUB_CT_LABEL)
+    covariates_minor = bulk_group + [SUB_CT_LABEL]
     adata_bulk_minor_celltypes = bulkify_func(adata, covariates=covariates_minor)
     adata_bulk_minor_celltypes = normalize(adata_bulk_minor_celltypes)
     adata_bulk_minor_celltypes.write(args.bulk_minor_celltype)
